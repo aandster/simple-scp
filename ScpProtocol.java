00ecff01d3d85a5129957574cc392e6b6ffd47c7
Jordan Maddock <jordan.maddock@gmail.com>
2018-08-31 22:19:08 +1000

    Created SCP Protocol class. Current implementation has methods for: * Formatting SCP messages from given method arguments * Validation for host names and port numbers

---
+++ ScpProtocol.java
@@ -0,0 +1,141 @@
+// SCP Worker
+// Author: Jordan Maddock
+// Date: 31/08/2018
+// This contains methods that are used to format SCP message strings.
+
+public class ScpProtocol {
+
+    /**
+     * Takes hostname and returns true if it is valid
+     *
+     * @param hostname a hostname
+     * @return boolean true if valid
+     */
+    public static boolean isValidHostname(String hostname) {
+        return hostname.matches("[0-9][a-z][A-Z]+");
+    }
+
+    /**
+     * Takes a value for a port number and returns true if it is valid
+     * Port numbers should be > 1023 and < 65535, and only contain digits
+     *
+     * @param portnumber a port number value
+     * @return true if value is a valid port number
+     */
+    public static boolean isValidPort(String portnumber) {
+        return (portnumber.matches("[0-9]") &&
+                Integer.parseInt(portnumber) > 1023 &&
+                Integer.parseInt(portnumber) < 65535
+        );
+    }
+
+    /**
+     * Formats a SCP CONNECT message
+     *
+     * @param server_address  destination hostname
+     * @param server_port     destination port
+     * @param request_created time request created
+     * @param username        client username
+     * @return formatted string
+     */
+    public static String CONNECT(String server_address, int server_port, int request_created, String username) {
+        return ("SCP CONNECT\n" +
+                "SERVERADDRESS " + server_address + "\n" +
+                "SERVERPORT " + server_port + "\n" +
+                "REQUESTCREATED " + request_created + "\n" +
+                "USERNAME \"" + username + "\"\n" +
+                "SCP END\n"
+        );
+    }
+
+    /**
+     * Formats a SCP REJECT message
+     *
+     * @param time_difference unacceptable time difference
+     * @param remote_address  address of client being rejected
+     * @return formatted string
+     */
+    public static String REJECT(int time_difference, String remote_address) {
+        return ("SCP REJECT\n" +
+                "TIMEDIFFERENTIAL " + time_difference + "\n" +
+                "REMOTEADDRESS " + remote_address + "\n" +
+                "SCP END\n"
+        );
+    }
+
+    /**
+     * Formats a SCP ACCEPT message
+     *
+     * @param username       username of client user
+     * @param client_address hostname of client
+     * @param client_port    port number used by client
+     * @return formatted string
+     */
+    public static String ACCEPT(String username, String client_address, int client_port) {
+        return ("SCP ACCEPT\n" +
+                "USERNAME \"" + username + "\"\n" +
+                "CLIENTADDRESS " + client_address + "\n" +
+                "CLIENTPORT " + client_port + "\n" +
+                "SCP END\n"
+        );
+    }
+
+    /**
+     * Formats a SCP ACKNOWLEDGE message
+     *
+     * @param username       client username
+     * @param server_address server hostname
+     * @param server_port    server receiving port
+     * @return formatted string
+     */
+    public static String ACKNOWLEDGE(String username, String server_address, int server_port) {
+        return ("SCP ACKNOWLEDGE\n" +
+                "USERNAME \"" + username + "\"\n" +
+                "SERVERADDRESS " + server_address + "\n" +
+                "SERVERPORT " + server_port + "\n" +
+                "SCP END\n"
+        );
+    }
+
+    /**
+     * Formats a short SCP ACKNOWLEDGE message
+     *
+     * @return formatted string
+     */
+    public static String ACKNOWLEDGE() {
+        return ("SCP ACKNOWLEDGE\n" +
+                "SCP END\n"
+        );
+    }
+
+    /**
+     * Formats a SCP CHAT message
+     * The message contents should not contain "SCP END"
+     *
+     * @param remote_address   destination hostname
+     * @param remote_port      destination port
+     * @param message_contents contents of chat message
+     * @return formatted string
+     */
+    public static String CHAT(String remote_address, int remote_port, String message_contents) {
+        return ("SCP CHAT\n" +
+                "REMOTEADDRESS " + remote_address + "\n" +
+                "REMOTEPORT " + remote_port + "\n" +
+                "MESSAGECONTENT\n" +
+                "\n\n" +
+                message_contents + "\n" +
+                "SCP END\n"
+        );
+    }
+
+    /**
+     * Formats a SCP DISCONNECT message
+     *
+     * @return formatted string
+     */
+    public static String DISCONNECT() {
+        return ("SCP DISCONNECT\n" +
+                "SCP END\n"
+        );
+    }
+}
