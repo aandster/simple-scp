0389971a9e8e6924bbba1780fbef499f578cbb01
Jordan Maddock <jordan.maddock@gmail.com>
2018-08-31 20:04:02 +1000

    Commented in steps to be done in execution

--- ChatClient.java
+++ ChatClient.java
@@ -4,5 +4,24 @@
 // This is the client for the simple-scp project.
 
 public class ChatClient {
-	
+
+    public static void main(String[] args) {
+        // Check if two arguments have been given (host, port)
+        // If there are arguments missing, assign default values
+        // If there are arguments there, check them for correctness and validity
+        // Assign the values of the arguments to variables
+
+        // create the socket
+
+        // send connection request to server
+
+        // if rejection message is received, exit the program
+
+        // if acceptance message is received, send an acknowledgement
+
+        // enter receive and then chat or disconnect cycle
+
+        // wait for chat to be received
+    }
+
 }
--- ChatServer.java
+++ ChatServer.java
@@ -4,5 +4,25 @@
 // This is the server for the simple-scp project.
 
 public class ChatServer {
-	
+
+    public static void main(String[] args) {
+        // Check if three arguments have been given (host, port, message)
+        // If there are arguments missing, assign default values
+        // If there are arguments there, check them for correctness and validity
+        // Assign the values of the arguments to variables
+
+        // create the socket server
+
+        // wait for connection request
+
+        // when receiving connection request, check for time difference
+        // if time diff is greater than five seconds, send a rejection
+        // if the time difference is acceptable, send an acceptance message
+
+        // wait for acknowledgement
+        // send the welcome message
+
+        // enter receive and then chat or disconnect cycle
+
+    }
 }
