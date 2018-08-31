56800e0506840a99290f7e2e7838d6f48a01e354
Jordan Maddock <jordan.maddock@gmail.com>
2018-08-31 13:22:13 +1000

    Created ChatServer and ChatClient source files

---
+++ ChatClient.java
@@ -0,0 +1,8 @@
+// ChatClient
+// Author: Jordan Maddock
+// Date: 31/08/2018
+// This is the client for the simple-scp project.
+
+public class ChatClient {
+	
+}
---
+++ ChatServer.java
@@ -0,0 +1,8 @@
+// ChatServer
+// Author: Jordan Maddock
+// Date: 31/08/2018
+// This is the server for the simple-scp project.
+
+public class ChatServer {
+	
+}
