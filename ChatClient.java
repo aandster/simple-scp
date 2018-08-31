8c47899bf94e36596462d24b2be6a0e69c1f8589
Jordan Maddock <jordan.maddock@gmail.com>
2018-08-31 23:00:57 +1000

    Moved code for program argument checking from main methods of ChatClient and ChatServer to ScpProtocol in the isValid* methods, and updated the implementation of these methods in ChatServer and ChatClient.
    The isValid* methods will still return a boolean value indicating the validity of passed arguments, but with the addition of printing error messages to the output terminal.
    
    These is intended to be used when deciding whether to terminate a Chat* program on initialisation.

--- ChatClient.java
+++ ChatClient.java
@@ -3,13 +3,32 @@
 // Date: 31/08/2018
 // This is the client for the simple-scp project.
 
+import java.io.IOException;
+
 public class ChatClient {
 
-    public static void main(String[] args) {
-        // Check if two arguments have been given (host, port)
-        // If there are arguments missing, assign default values
-        // If there are arguments there, check them for correctness and validity
-        // Assign the values of the arguments to variables
+    public static void main(String[] args) throws IOException {
+        String HOSTNAME;
+        int PORT_NUMBER;
+
+        // Check that correct number of arguments are given, validate and save arguments
+        if (args.length == 2) {
+            // Check that hostname is valid
+            if (ScpProtocol.isValidHostname(args[0])) {
+                HOSTNAME = args[0];
+            } else {
+                return;
+            }
+            // Check that port number is valid
+            if (ScpProtocol.isValidPort(args[1])) {
+                PORT_NUMBER = Integer.parseInt(args[1]);
+            } else {
+                return;
+            }
+        } else {
+            HOSTNAME = ScpProtocol.default_hostname;
+            PORT_NUMBER = ScpProtocol.default_port;
+        }
 
         // create the socket
 
